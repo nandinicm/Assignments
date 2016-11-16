14fdfbce6bdb21848841c1d611e1fa1110d88892
nandinicm <nandini.cm@razorthink.net>
2016-11-16 16:40:53 +0530

    new commit

---
+++ MyClass.java
@@ -0,0 +1,10 @@
+package FirstPackage;
+
+/**
+ * Created by nandinicm on 16/11/16.
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
